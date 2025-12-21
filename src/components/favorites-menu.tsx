3f8ce16f3372c0162f5fc17bdb12de72d17b4652
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-21 03:25:51 +0000

    como eu insiro o item adicionado a favoritos?

--- src/components/favorites-menu.tsx
+++ src/components/favorites-menu.tsx
@@ -19,6 +19,31 @@ interface FavoritesMenuProps {
 }
 
 export default function FavoritesMenu({ favoriteClients, onSelectClient }: FavoritesMenuProps) {
-  // Always return null to effectively hide and "clear" the favorites from the UI.
-  return null;
+  if (favoriteClients.length === 0) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button 
+          type="button" 
+          variant="outline"
+          size="icon"
+          className="h-10 w-10 sm:h-12 sm:w-12 shrink-0"
+        >
+          <Star className="h-5 w-5 text-amber-500" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Clientes Favoritos</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {favoriteClients.map((client) => (
+          <DropdownMenuItem key={client.id} onSelect={() => onSelectClient(client)}>
+            {client.name}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
