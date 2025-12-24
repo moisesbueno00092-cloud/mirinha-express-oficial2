166d057cadca13dc6544410f1919345d55afb9de
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-24 19:39:36 +0000

    vamos excluir todo contuedo da aba despesas, sem prejudicar nosso app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -701,12 +701,6 @@ export default function Home() {
                        Caderneta
                     </Button>
                 </Link>
-                 <Link href="/finance" passHref>
-                    <Button variant="outline" size="sm">
-                       <DollarSign className="mr-2 h-4 w-4" />
-                       Despesas
-                    </Button>
-                </Link>
             </div>
             <div className="text-right">
                 <span className="text-muted-foreground">Faturamento Total:</span>
