784a26141a8d0723cf7037216f609c34ee608a10
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-27 11:08:49 +0000

    vc pode uasr um modelo moderno desse calendario, com o nome dos dias ali

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -34,7 +34,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "flex w-full mt-2",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
@@ -67,4 +67,5 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
+
 
