c9fb365b15b840dbd2c2672dd7b5282cb2b64b28
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-27 11:23:43 +0000

    qual a dificuldade em modificar o calendario

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,8 +2,10 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, DropdownProps } from "react-day-picker"
 import { ptBR } from 'date-fns/locale';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -16,6 +18,15 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
+  const handleSelectChange = (
+    onchange: (value: string) => void | undefined,
+    value: string
+  ) => {
+    if (onchange) {
+      onchange(value);
+    }
+  };
+
   return (
     <DayPicker
       locale={ptBR}
@@ -25,7 +36,8 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex justify-center gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -56,6 +68,35 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Dropdown: ({ value, onChange, children, ...rest }: DropdownProps) => {
+          const options = React.Children.toArray(children) as React.ReactElement<React.HTMLProps<HTMLOptionElement>>[];
+          const selected = options.find((child) => child.props.value === value);
+          
+          return (
+            <Select
+              value={value?.toString()}
+              onValueChange={(value) => {
+                handleSelectChange(onChange, value);
+              }}
+            >
+              <SelectTrigger className="pr-1.5 focus:ring-0 w-fit">
+                <SelectValue>{selected?.props?.children}</SelectValue>
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <ScrollArea className="h-80">
+                  {options.map((option, id: number) => (
+                    <SelectItem
+                      key={`${id}-${option.props.value}`}
+                      value={option.props.value?.toString() ?? ''}
+                    >
+                      {option.props.children}
+                    </SelectItem>
+                  ))}
+                </ScrollArea>
+              </SelectContent>
+            </Select>
+          )
+        }
       }}
       {...props}
     />
