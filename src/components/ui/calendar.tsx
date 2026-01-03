7cb0835897d3673c55dbe9023aea79adecbd8ca7
moisés Bueno <moisesbueno00092@gmail.com>
2026-01-03 23:00:01 +0000

    n

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,7 +3,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DropdownProps, CaptionProps } from "react-day-picker"
+import { DayPicker, DropdownProps, CaptionProps, HeadProps } from "react-day-picker"
 import { ptBR } from 'date-fns/locale';
 import { format } from 'date-fns';
 
@@ -13,6 +13,26 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { ScrollArea } from "./scroll-area";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
+
+function CustomHead({ ...props }: HeadProps) {
+    const { locale, classNames, ...rest } = props;
+    const weekdays = Array.from({ length: 7 }, (_, i) => {
+        const date = new Date(Date.UTC(2023, 0, 1 + i));
+        return format(date, 'ccc', { locale: props.locale });
+    });
+
+    return (
+        <thead {...rest} className={cn("text-muted-foreground", classNames?.head)}>
+            <tr className="flex">
+                {weekdays.map((weekday, index) => (
+                    <th key={index} scope="col" className="flex h-9 w-9 items-center justify-center p-0 font-normal text-[0.8rem]">
+                        {weekday}
+                    </th>
+                ))}
+            </tr>
+        </thead>
+    );
+}
 
 function Calendar({
   className,
@@ -41,7 +61,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex items-center justify-center",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
@@ -62,6 +82,7 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Head: CustomHead,
         Dropdown: ({ value, onChange, children, ...props }: DropdownProps) => {
           const options = React.Children.toArray(
             children
