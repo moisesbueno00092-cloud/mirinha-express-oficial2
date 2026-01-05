082d8617d857323ab06f7c52e1a63c4c5faa14d0
moisés Bueno <moisesbueno00092@gmail.com>
2026-01-05 02:19:00 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -590,7 +590,7 @@ originalGroup = group;
   }, [todaysItems]);
   
   
-  if (isUserLoading) {
+  if (isUserLoading || !user) {
     return (
       <div className="flex h-screen items-center justify-center">
         <Loader2 className="h-12 w-12 animate-spin text-primary" />
