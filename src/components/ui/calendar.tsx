d4d2fd74764269f09324dc247067f0896993cf3e
moisés Bueno <moisesbueno00092@gmail.com>
2026-02-01 21:35:51 +0000

    nada mudou

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,7 +32,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "hidden",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
@@ -56,7 +56,6 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-        Head: () => null,
       }}
       {...props}
     />
