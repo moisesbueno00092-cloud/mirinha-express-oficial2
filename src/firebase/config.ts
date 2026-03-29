8c6a4dc28c1a95a6bbcaebcac9fa13b5309327a1
Moises <moises@example.com>
2026-03-28 21:20:43 -0300

    chore: Update Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-5553654367-98546",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:374860758714:web:09c2e02299b4f7df584c83",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCYm_FaDBY708Fn8IH-Oz3JuPdy1qv-47Q",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-5553654367-98546.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mirinha-express",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:219322430193:web:33b723d7bfe06c06a36f50",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyC00hHCf5tZtJS9DOOCAf8FdFndZsgIvwE",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mirinha-express.firebaseapp.com",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "374860758714"
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "219322430193"
 };
